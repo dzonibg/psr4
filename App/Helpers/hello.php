dcf6499423a8844f406d2e618d62777f4e08d17c
Nikola <nidza.bg@gmail.com>
2021-04-27 15:40:09 +0200

    First.

---
+++ App/Helpers/hello.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Helpers;
+
+class Greeter {
+    public function greet() {
+        echo "Hello!";
+    }
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Helpers\Greeter;
+
+require_once 'vendor/autoload.php';
+
+$greeter = new Greeter();
+$greeter->greet();
